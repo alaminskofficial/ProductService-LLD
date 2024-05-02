1b031c4d7d875946ef9d75c1e6ec935265491d95
Sk Alamin <alamin695sk@gmail.com>
2024-05-03 01:03:05 +0530

    category added

--- src/main/java/com/example/products/models/Category.java
+++ src/main/java/com/example/products/models/Category.java
@@ -1,12 +1,13 @@
 package com.example.products.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 
 import java.util.List;
 
 @Entity
 public class Category extends BaseModel{
-    @OneToMany(mappedBy = "category")
+    @OneToMany(mappedBy = "category" ,fetch = FetchType.EAGER)
     List<Product> products;
 }
